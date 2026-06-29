97744b7b9258e11baee155672537031d05f1b32d
David Furnes <david@dfurnes.com>
2026-06-28 20:31:06 -0400

    Scope down preview to save tokens.

--- preview.sh
+++ preview.sh
@@ -13,12 +13,12 @@ env_flag=$([ -f .env ] && echo "--env-file .env")
 
 podman run --rm $env_flag \
   -v "$PWD/data:/cache:z" \
-  diffedora --cache-dir /cache --releases 20 --variant silverblue --output html \
+  diffedora --cache-dir /cache --releases 5 --variant silverblue --output html \
   > "$site/index.html"
 
 podman run --rm $env_flag \
   -v "$PWD/data:/cache:z" \
-  diffedora --cache-dir /cache --releases 5 --variant coreos --output html \
+  diffedora --cache-dir /cache --releases 1 --variant coreos --output html \
   > "$site/coreos.html"
 
 echo "Serving at http://localhost:8080 (Ctrl+C to stop)"
